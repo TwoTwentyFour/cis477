8c0f8bc8c6cabf21c19f45680a0b4e7fc61776a1
root <root@ip-172-31-28-247.us-east-2.compute.internal>
2019-09-11 12:09:52 +0000

    added images directory

--- pages/index.php
+++ pages/index.php
@@ -15,25 +15,25 @@
                 </thead>
                 <tbody>
                   <tr>
-                    <td>1</td>
+                    <td><a href="read_order.php">1</a></td>
                     <td>4/9/2019</td>
                     <td>Sent</td>
-                    <td  style="float: right;"><a href="update_order.php">Edit</a></td>
-                    <td  style="float: right;"><a href="delete_order.php">Delete</a></td>
+                    <td style="float: right;"><a href="update_order.php">Edit</a></td>
+                    <td style="float: right;"><a href="delete_order.php">Delete</a></td>
                   </tr>
                   <tr>
-                    <td>2</td>
+                    <td><a href="read_order.php">2</a></td>
                     <td>6/12/2019</td>
                     <td>Sent</td>
-                    <td  style="float: right;"><a href="update_order.php">Edit</a></td>
-                    <td  style="float: right;"><a href="delete_order.php">Delete</a></td>
+                    <td style="float: right;"><a href="update_order.php">Edit</a></td>
+                    <td style="float: right;"><a href="delete_order.php">Delete</a></td>
                   </tr>
                   <tr>
-                    <td>3</td>
+                    <td><a href="read_order.php">3</a></td>
                     <td>9/9/2019</td>
                     <td style="color: tomato;">Pending!</td>
-                    <td  style="float: right;"><a href="update_order.php">Edit</a></td>
-                    <td  style="float: right;"><a href="delete_order.php">Delete</a></td>
+                    <td style="float: right;"><a href="update_order.php">Edit</a></td>
+                    <td style="float: right;"><a href="delete_order.php">Delete</a></td>
                   </tr>
                 </tbody>
               </table>
