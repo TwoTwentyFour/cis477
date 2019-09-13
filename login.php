05159e8b6ab30523b7feda0efc85d6e889c6eec3
root <root@ip-172-31-28-247.us-east-2.compute.internal>
2019-09-13 15:14:03 +0000

    Password check should be working now.

--- login.php
+++ login.php
@@ -1,36 +1,54 @@
 <?php  
 
+/** Just checking to see if the submit button has been pressed.**/
 if (isset($_POST['login-submit']))
 {
     require 'database.connection.php';
 
+    /** Storing the users input from the form
+        below into PHP variables. **/
     $user_email = $_POST['mail'];
     $user_password = $_POST['pword'];
-    $sql = "SELECT * FROM users WHERE user_email = '" . $user_emailha . "' AND user_password = '" . SHA1($user_password) . "';";
 
-    if (mysqli_query($connection, $sql))
+    /** Writing an SQL statement and sending
+        it off to the database. **/
+    // TODO: SQL Injection Warning! Change to a prepared statment!!!
+    $sql = "SELECT * FROM users WHERE user_email = '" . $user_email . "' AND user_password = '" . SHA1($user_password) . "';";
+
+    if ($results = mysqli_query($connection, $sql))
     {
-        echo("<h4 class=\"todo\">Fifth Check</h4>");
-        header("Location: ./pages");
+        if ($row = mysqli_fetch_array($results, MYSQLI_ASSOC))
+        {
+            header("Location: ./pages");
+        }
+        else 
+        {
+            readfile('./templates/header.html');
+            echo ("<small class=\"todo\">Seems like either your use name or password is incorrect.<br>Please try again.</small>");
+            readfile('./templates/footer.html');
+        }
     }
     else
     {
+
+        readfile('./templates/header.html');
         echo("Error: " . mysqli_error($connection));
+        readfile('./templates/footer.html');
     }
 
 }
 else
 {
+    /** If the Submit button hasn't been hit yet,
+        we'll serve the following HTML. **/ 
     readfile('./templates/header.html');
 
     echo ('
-
         <form action="login.php" method="post">
             <input type="email" name="mail" placeholder="Email" required="required">
             <input type="password" name="pword" placeholder="Password" required="required">
             <button class="btn btn-success" type="submit" name="login-submit">Login</button>
         </form>
-
     ');
     
     readfile('./templates/footer.html');
--- register.php
+++ register.php
@@ -1,23 +1,45 @@
 <?php 
 
+/** Just checking to see if the submit button has been pressed.**/
 if (isset($_POST['register-submit']))
 {
-
     require 'database.connection.php';
 
+    /** Storing the users input from the form
+        below into PHP variables. **/
     $user_email = $_POST['mail'];
     $user_password = $_POST['pword'];
-    $protected_password = SHA1($user_password);
     $password_check = $_POST['pword-check'];
 
-    $sql = "INSERT INTO users (user_email, user_password) VALUES ('$user_email', '$protected_password');";
-    mysqli_query($connection, $sql);
-
-    header("Location: ./pages/");
-
+    if ($user_password == $password_check)
+    {
+        $protected_password = SHA1($user_password);
+        /** Writing an SQL statement and sending
+            it off to the database. **/
+        // TODO: SQL Injection Warning! Change to a prepared statment!!!
+        $sql = "INSERT INTO users (user_email, user_password) VALUES ('$user_email', '$protected_password');"; 
+        if (mysqli_query($connection, $sql))
+        {
+            header("Location: ./pages/");
+        }
+        else
+        {
+             readfile('./templates/header.html');
+             echo("Error: " . mysqli_error($connection));
+             readfile('./templates/footer.html');
+        }
+    }
+    else
+    {
+        readfile('./templates/header.html');
+        echo ("<small class=\"todo\">Both password entries must match.<br>Please try again.</small>");
+        readfile('./templates/footer.html');
+    }
 }
 else
 {
+    /** If the Submit button hasn't been hit yet,
+        we'll serve the following HTML. **/ 
     readfile('./templates/header.html');
 
     echo ('
@@ -27,7 +49,6 @@ else
             <input type="password" name="pword-check" placeholder="Re-Type Password" required="required">
             <button class="btn btn-success" name="register-submit" type="submit">Register</button>
         </form>
-
     ');
 
     readfile('./templates/footer.html');
